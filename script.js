cf7b820166864cb161e3590589ae7b664de8d2c2
Victor Moreira <136837606+victormoreirak@users.noreply.github.com>
2023-08-03 16:45:36 -0300

    tornando o projeto responsivo

--- script.js
+++ script.js
@@ -6,3 +6,6 @@ const moveOverlay = () => loginContainer.classList.toggle('move')
 
 document.getElementById('open-register').addEventListener('click', moveOverlay)
 document.getElementById('open-login').addEventListener('click', moveOverlay)
+
+document.getElementById('open-register-mobile').addEventListener('click', moveOverlay)
+document.getElementById('open-login-mobile').addEventListener('click', moveOverlay)
